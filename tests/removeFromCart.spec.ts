bbce5e2a6c1fd3c463a799343ea299cd13f62b80
Mohammed AbuZahra <mohammedabuzahra@Mohammeds-MacBook-Pro.local>
2025-05-24 20:52:18 +0300

    adding add to cart and remove from cart test cases with some hooks and grouping and parameterized test

--- tests/removeFromCart.spec.ts
+++ tests/removeFromCart.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
-test.describe('Add To Cart Using Standard Username', () => {
+test.describe('Add Then Remove From Cart Using Standard Username', () => {
     test.beforeEach(async ({ page }) => {
         const loginPage = new LoginPage(page);
         await loginPage.goto();
@@ -11,31 +11,31 @@ test.describe('Add To Cart Using Standard Username', () => {
         await page.waitForTimeout(3000);
         await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
     });
-    test('should add an item to the cart', async ({ page }) => {
+    test('should add an item to the cart and remove it', async ({ page }) => {
         await page.click('[data-test="add-to-cart-sauce-labs-fleece-jacket"]');
         await page.waitForTimeout(2000);
+        await page.click('[data-test="remove-sauce-labs-fleece-jacket"]');
         await page.click('.shopping_cart_link');
         await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
-        await expect(page.locator('.cart_item')).toHaveCount(1);
-        await expect(page.locator('[data-test="cart-list"]')).toContainText('Sauce Labs Fleece Jacket');
-
+        await expect(page.locator('.cart_item')).toHaveCount(0);
     });
 
-    test('should add two items to the cart', async ({ page }) => {
+    test('should add two items to the cart and remove them', async ({ page }) => {
         await page.click('[data-test="add-to-cart-sauce-labs-fleece-jacket"]');
+        await page.waitForTimeout(2000);
+        await page.click('[data-test="remove-sauce-labs-fleece-jacket"]');
         await page.waitForTimeout(2000);
         await page.click('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]');
         await page.waitForTimeout(2000);
+        await page.click('[data-test="remove-sauce-labs-bolt-t-shirt"]');
         await page.click('.shopping_cart_link');
         await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
-        await expect(page.locator('.cart_item')).toHaveCount(2);
-        await expect(page.locator('[data-test="cart-list"]')).toContainText('Sauce Labs Fleece Jacket');
-        await expect(page.locator('[data-test="cart-list"]')).toContainText('Sauce Labs Bolt T-Shirt');
+        await expect(page.locator('.cart_item')).toHaveCount(0);
 
     });
 });
 
-test.describe('Add To Cart Using Problem Username', () => {
+test.describe('Add To Cart Using Problem Username and remove it', () => {
     test.beforeEach(async ({ page }) => {
         const loginPage = new LoginPage(page);
         await loginPage.goto();
@@ -45,13 +45,14 @@ test.describe('Add To Cart Using Problem Username', () => {
         await page.waitForTimeout(3000);
         await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
     });
-    test.fail('Test is not adding the Sauce Labs Fleece Jacket ', async ({ page }) => {
+    test.fail('Test is not adding the Sauce Labs Fleece Jacket so not removing it ', async ({ page }) => {
         await page.click('[data-test="add-to-cart-sauce-labs-fleece-jacket"]');
         await page.waitForTimeout(2000);
+        await expect(page.locator('[data-test="remove-sauce-labs-fleece-jacket"]')).toBeVisible();
+        await page.click('[data-test="remove-sauce-labs-fleece-jacket"]');
         await page.click('.shopping_cart_link');
         await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
-        await expect(page.locator('.cart_item')).toHaveCount(1);
-        await expect(page.locator('[data-test="cart-list"]')).toContainText('Sauce Labs Fleece Jacket');
+        await expect(page.locator('.cart_item')).toHaveCount(0);
     });
 
 });
@@ -66,13 +67,14 @@ test.describe('Add To Cart Using Error Username', () => {
         await page.waitForTimeout(3000);
         await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
     });
-    test.fail('Test is not adding the Sauce Labs Fleece Jacket ', async ({ page }) => {
+    test.fail('Test is not adding the Sauce Labs Fleece Jacket so not removing it ', async ({ page }) => {
         await page.click('[data-test="add-to-cart-sauce-labs-fleece-jacket"]');
         await page.waitForTimeout(2000);
+        await expect(page.locator('[data-test="remove-sauce-labs-fleece-jacket"]')).toBeVisible();
+        await page.click('[data-test="remove-sauce-labs-fleece-jacket"]');
         await page.click('.shopping_cart_link');
         await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
-        await expect(page.locator('.cart_item')).toHaveCount(1);
-        await expect(page.locator('[data-test="cart-list"]')).toContainText('Sauce Labs Fleece Jacket');
+        await expect(page.locator('.cart_item')).toHaveCount(0);
     });
 
 });
